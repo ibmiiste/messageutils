f372886be17856cd1505180e08932f14baaa523b
Olivier Drubigny <olivier.drubigny@ista.com>
2025-02-11 13:52:03 +0100

    Suppress .git folder

--- install_deps.py
+++ install_deps.py
@@ -2,16 +2,20 @@ import os
 import subprocess
 import json
 import shutil
+import stat
 
 def clone_or_update(repo_name, repo_info, base_dir):
     repo_path = os.path.join(base_dir, repo_name)
     
-    if not os.path.exists(repo_path):
-        print(f"Clonage de {repo_name} depuis {repo_info['url']}...")
-        subprocess.run(["git", "clone", repo_info["url"], repo_path], check=True)
-    else:
-        print(f"Mettre à jour {repo_name}...")
-        subprocess.run(["git", "-C", repo_path, "pull"], check=True)
+    # Supprimer le dossier du projet s'il existe déjà
+    if os.path.exists(repo_path):
+        print(f"Suppression du dossier existant pour {repo_name}...")
+        shutil.rmtree(repo_path)
+        print(f"Dossier existant supprimé pour {repo_name}.\n")
+    
+    # Cloner ou mettre à jour le dépôt
+    print(f"Clonage de {repo_name} depuis {repo_info['url']}...")
+    subprocess.run(["git", "clone", repo_info["url"], repo_path], check=True)
     
     print(f"Basculer vers la référence {repo_info['ref']} pour {repo_name}...")
     subprocess.run(["git", "-C", repo_path, "checkout", repo_info["ref"].strip()], check=True)
@@ -20,31 +24,35 @@ def clone_or_update(repo_name, repo_info, base_dir):
     
     # Supprimer le dossier .vscode s'il existe
     vscode_path = os.path.join(repo_path, ".vscode")
-    if (os.path.exists(vscode_path)):
+    if os.path.exists(vscode_path):
         print(f"Suppression du dossier .vscode pour {repo_name}...")
         shutil.rmtree(vscode_path)
         print(f"Dossier .vscode supprimé pour {repo_name}.\n")
     
     # Supprimer le fichier iproj.json s'il existe
     iproj_path = os.path.join(repo_path, "iproj.json")
-    if (os.path.exists(iproj_path)):
+    if os.path.exists(iproj_path):
         print(f"Suppression du fichier iproj.json pour {repo_name}...")
         os.remove(iproj_path)
         print(f"Fichier iproj.json supprimé pour {repo_name}.\n")
     
     # Supprimer le fichier Rules.mk s'il existe
     rules_mk_path = os.path.join(repo_path, "Rules.mk")
-    if (os.path.exists(rules_mk_path)):
+    if os.path.exists(rules_mk_path):
         print(f"Suppression du fichier Rules.mk pour {repo_name}...")
         os.remove(rules_mk_path)
         print(f"Fichier Rules.mk supprimé pour {repo_name}.\n")
+    
+    # Supprimer le dossier .git s'il existe
+    git_path = os.path.join(repo_path, ".git")
+    if os.path.exists(git_path):
+        print(f"Suppression du dossier .git pour {repo_name}...")
+        shutil.rmtree(git_path, onerror=remove_readonly)
+        print(f"Dossier .git supprimé pour {repo_name}.\n")
 
-    # Supprimer le fichier .gitignore s'il existe
-    gitignore_path = os.path.join(repo_path, ".gitignore")
-    if (os.path.exists(gitignore_path)):
-        print(f"Suppression du fichier Rules.mk pour {repo_name}...")
-        os.remove(gitignore_path)
-        print(f"Fichier .gitignore supprimé pour {repo_name}.\n")    
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
 
 def install_dependencies(dependencies_file, base_dir):
     """
